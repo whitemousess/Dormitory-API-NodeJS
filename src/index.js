893c4a51be0a41481f053837cec3a393596552e3
whitemousess <thangcoiofficial@gmail.com>
2023-09-20 08:44:02 +0700

    cors

--- src/index.js
+++ src/index.js
@@ -1,35 +1,26 @@
-const createError = require('http-errors');
-const express = require('express')
+const express = require('express');
+const bodyParser = require('body-parser')
+const cors = require('cors');
 const db = require('./config/db')
-const route = require("./routes");
-const dotenv = require('dotenv');
+const route = require('./routes');
+const cookieParser = require("cookie-parser");
+const dotenv = require("dotenv");
 dotenv.config();
- 
-const app = express()
-const port = 1407
+
+const app = express();
+const port = 1407;
 
 db.connect();
 
-app.use(express.json());
+app.use(cors());
+// for parsing application/json
+app.use(bodyParser.json()); 
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true })); 
+// read token in cookie
+app.use(cookieParser());
 
 route(app);
-
-//catch 404 and forward to error handler
-app.use(function(req, res,next){
-    next(createError(404));
-});
-
-// error handler
-
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development'? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 app.listen(port, () =>
   console.log(`app listening at http://localhost:${port}`)
